0d963cf15d17c46b57a393838ffeb2ed9a2a72f1
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2023-04-23 22:31:06 +0530

    product upload

--- routes/payment.js
+++ routes/payment.js
@@ -5,6 +5,44 @@ const crypto = require("crypto");
 const { readFileSync } = require("fs");
 const prisma = require("../lib/prisma");
 const path = require("path");
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
+async function getProducts(sheetTitle) {
+  if (
+    !(
+      process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL &&
+      process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY &&
+      process.env.GOOGLE_SPREADSHEET_PRODUCTS
+    )
+  ) {
+    throw new Error("forbidden");
+  }
+
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_PRODUCTS);
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(
+      /\\n/gm,
+      "\n"
+    ),
+  });
+  await doc.loadInfo();
+  const sheet = doc.sheetsByTitle[sheetTitle]; // or use doc.sheetsById[id]
+  const rows = await sheet.getRows(); // can pass in { limit, offset }
+
+  const products = rows?.map(
+    ({ id, name, slug, price, sale_price, unit, quantity_in_stock }) => ({
+      id,
+      name,
+      slug,
+      price,
+      sale_price,
+      unit,
+      quantity_in_stock,
+    })
+  );
+  return products;
+}
 
 router.post("/razorpay/create", async (req, res) => {
   const {
@@ -19,25 +57,27 @@ router.post("/razorpay/create", async (req, res) => {
     description,
   } = req.body;
 
-  const data = readFileSync(
-    path.join(__dirname, "../upload/products.json"),
-    "utf-8"
-  );
-  const products = JSON.parse(data);
+  // const data = readFileSync(
+  //   path.join(__dirname, "../upload/products.json"),
+  //   "utf-8"
+  // );
+  // const products = JSON.parse(data);
+
+  const products = await getProducts("all_items");
 
   const total = item.reduce((acc, itm) => {
-    const selectedProduct = products.find((product) => product.id === itm.id);
+    const selectedProduct = products.find((product) => {
+      return product.id === itm.id && product.slug === itm.slug;
+    });
 
     const amtSum = Math.round(
-      parseFloat(selectedProduct.sale_price) * parseInt(itm.quantity)
+      Number(selectedProduct.sale_price) * Number(itm.quantity)
     ).toFixed(2);
 
-    return acc + amtSum;
+    return Number(acc) + Number(amtSum);
   }, 0);
 
-  const netAmount = Math.round(
-    parseFloat(total) + parseFloat(shipping)
-  ).toFixed(2);
+  const netAmount = Math.round(Number(total) + Number(shipping)).toFixed(2);
 
   const razorpay = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
@@ -71,7 +111,7 @@ router.post("/razorpay/create", async (req, res) => {
         mobile: mobile,
         pincode: pincode,
         description: description,
-        amount: total,
+        amount: JSON.stringify(total),
         discount: discount == 0 ? "0" : discount,
         shipping: shipping == 0 ? "free" : shipping,
         totalAmount: netAmount,
